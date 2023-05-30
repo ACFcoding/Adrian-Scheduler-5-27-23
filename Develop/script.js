743994660583a716f6589cb7ad31bb7624b3e379
ACFcoding <adrianc.franks@gmail.com>
2023-05-29 22:15:11 -0600

    Resolved display issues

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,44 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 $(document).ready(function() {
 
 //Grabbed updating current day
 var date = dayjs();
 $('#currentDay').text(date.format('dddd, MMM D'));
 
-//var hour = dayjs().hour();
-//console.log(hour);
-//if (hour) {
- // setItem('.present')
-//}
+var hour = dayjs().hour();
+console.log(hour);
+var timeOfDay = ['.time-block'];
+if (timeOfDay = hour - 9) {
+  setItem('.present')
+} else if (timeOfDay < hour - 9) {
+  setItem('.past')
+}
+else (timeOfDay > hour - 9) {
+  setItem('.future')
+}
 
 
-var savedData = localStorage.getItem('hour-9', 'hour-10', 'hour-11', 'hour-13', 'hour-14', 'hour-15', 'hour-16', 'hour-17');
-console.log(savedData);
+//call saved data to display what the user saved
+var savedData = localStorage.getItem('hour-9');
+$('#hour-9 textarea.description').text(savedData);
+var savedData = localStorage.getItem('hour-10');
+$('#hour-10 textarea.description').text(savedData);
+var savedData = localStorage.getItem('hour-11');
+$('#hour-11 textarea.description').text(savedData);
+var savedData = localStorage.getItem('hour-12');
+$('#hour-12 textarea.description').text(savedData);
+var savedData = localStorage.getItem('hour-13');
+$('#hour-13 textarea.description').text(savedData);
+var savedData = localStorage.getItem('hour-14');
+$('#hour-14 textarea.description').text(savedData);
+var savedData = localStorage.getItem('hour-15');
+$('#hour-15 textarea.description').text(savedData);
+var savedData = localStorage.getItem('hour-16');
+$('#hour-16 textarea.description').text(savedData);
+var savedData = localStorage.getItem('hour-17');
+$('#hour-17 textarea.description').text(savedData);
+
+
 
 
 
@@ -33,38 +56,13 @@ saveUserData.addEventListener('click', function(event) {
 
   localStorage.setItem(timeBlock, textInput);
 
-
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 });
 
 
 
 
-//for DOM
+
 });
-
-
-  //var currentTime = dayjs();
-  //$('.container-fluid px-5').text(currentTime.format('A'));
-  //console.log(currentTime);
 
 
 });
